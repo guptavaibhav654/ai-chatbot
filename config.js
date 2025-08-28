9925568bae0268467bd5b950d3042285227ec1c7
guptavaibhav654 <work.vaibhav06@gmail.com>
2025-08-28 21:32:17 +0530

    config change

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Using environment variables for sensitive data
 
 const config = {
-    apiKey: 'YOUR_ACTUAL_API_KEY', // Replace this with your actual API key
+    apiKey: 'AIzaSyA55TACPBAWDM8uSnFqZsMhQGWMpl0Jypo', // Replace this with your actual API key
 };
 
 export default config;
